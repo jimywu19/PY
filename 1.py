3b1df53e7d2d88c8cbe2c73de2615255c169402f
jimywu19 <fqy471@qq.com>
2020-05-13 20:22:03 +0800

    删

--- 1.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#root = '%s%s%s' % ('..', os.path.sep, 'food')
-dir = 'c:/users/jimywu/documents/project/food'
-for  file_list in os.walk(dir):
-    for name in file_list[2]:
-        print('File: ' + name)
