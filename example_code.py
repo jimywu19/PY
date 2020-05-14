4951f7385aceb84b2f14479b5ed58b1ec76726bf
jimywu19 <fqy471@qq.com>
2020-05-14 09:31:30 +0800

    小改动

---
+++ example_code.py
@@ -0,0 +1,84 @@
+###### robotparser：解析robots.txt的工具
+
+from urllib import robotparser
+
+parser = robotparser.RobotFileParser()
+parser.set_url('https://www.taobao.com/robots.txt')
+parser.read()
+parser.can_fetch('Baiduspider', 'http://www.taobao.com/article')
+True
+parser.can_fetch('Baiduspider', 'http://www.taobao.com/product')
+False
+
+
+###### 通过下面的字典避免重复抓取并控制抓取深度
+
+def start_crawl(seed_url, match_pattern, *, max_depth=-1):
+    """开始执行爬虫程序并对指定的数据进行持久化操作"""
+    conn = pymysql.connect(host='localhost', port=3306,
+                           database='crawler', user='root',
+                           password='123456', charset='utf8')
+    try:
+        with conn.cursor() as cursor:
+            url_list = [seed_url]
+            # 通过下面的字典避免重复抓取并控制抓取深度
+            visited_url_list = {seed_url: 0}
+            while url_list:
+                current_url = url_list.pop(0)
+                depth = visited_url_list[current_url]
+                if depth != max_depth:
+                    # 尝试用utf-8/gbk/gb2312三种字符集进行页面解码
+                    page_html = get_page_html(current_url, charsets=('utf-8', 'gbk', 'gb2312'))
+                    links_list = get_matched_parts(page_html, match_pattern)
+                    param_list = []
+                    for link in links_list:
+                        if link not in visited_url_list:
+                            visited_url_list[link] = depth + 1
+                            page_html = get_page_html(link, charsets=('utf-8', 'gbk', 'gb2312'))
+                            headings = get_matched_parts(page_html, r'<h1>(.*)<span')
+                            if headings:
+                                param_list.append((headings[0], link))
+                    cursor.executemany('insert into tb_result values (default, %s, %s)',
+                                       param_list)
+                    conn.commit()
+
+
+
+#mysql 存取
+
+import pymysql
+from pymysql import Error
+
+def start_crawl(seed_url, match_pattern, *, max_depth=-1):
+    """开始执行爬虫程序并对指定的数据进行持久化操作"""
+    conn = pymysql.connect(host='localhost', port=3306,
+                           database='crawler', user='root',
+                           password='123456', charset='utf8')
+    try:
+        with conn.cursor() as cursor:
+            url_list = [seed_url]
+            # 通过下面的字典避免重复抓取并控制抓取深度
+            visited_url_list = {seed_url: 0}
+            while url_list:
+                current_url = url_list.pop(0)
+                depth = visited_url_list[current_url]
+                if depth != max_depth:
+                    # 尝试用utf-8/gbk/gb2312三种字符集进行页面解码
+                    page_html = get_page_html(current_url, charsets=('utf-8', 'gbk', 'gb2312'))
+                    links_list = get_matched_parts(page_html, match_pattern)
+                    param_list = []
+                    for link in links_list:
+                        if link not in visited_url_list:
+                            visited_url_list[link] = depth + 1
+                            page_html = get_page_html(link, charsets=('utf-8', 'gbk', 'gb2312'))
+                            headings = get_matched_parts(page_html, r'<h1>(.*)<span')
+                            if headings:
+                                param_list.append((headings[0], link))
+                    cursor.executemany('insert into tb_result values (default, %s, %s)',
+                                       param_list)
+                    conn.commit()
+    except Error:
+        pass
+        # logging.error('SQL:', error)
+    finally:
+        conn.close()
