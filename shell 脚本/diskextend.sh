3bd1f6c5d5305584ab0011cd708db580a852d896
jimywu19 <fqy471@qq.com>
2020-05-14 11:11:14 +0800

    删

--- shell 脚本/diskextend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp /etc/fstab /etc/fstab.bak2
-sed -i '/\/data/d' /etc/fstab
-umount /data
-lvremove -y /dev/mapper/centos-data
-lvextend -L 500G /dev/mapper/centos-root
-xfs_growfs /dev/mapper/centos-root
