7e2b5810db6faea388442472778963dbd2625a3e
jimywu19 <fqy471@qq.com>
2020-06-25 12:44:30 +0800

    小改动

--- shell 脚本/squidInstall.sh
+++ shell 脚本/squidInstall.sh
@@ -10,8 +10,8 @@ yum install -y squid httpd-tools
 #修改配置文件
 sed -i '/http_access deny all/d' "/etc/squid/squid.conf"
 sed -i 's/http_port 3128/http_port 31028/' "/etc/squid/squid.conf"
-cat <<EOF>>/etc/squid/squid.conf 
 
+cat >>/etc/squid/squid.conf <<EOF
 # require user auth 
 auth_param basic program /usr/lib64/squid/basic_ncsa_auth /etc/squid/passwd
 acl auth_user proxy_auth REQUIRED
@@ -20,7 +20,7 @@ http_access deny all
 EOF
 
 #增加用户
-htpasswd -bc /etc/squid/passwd zhangsanfeng z1VrlD46V1Gj  >>/dev/null 2>&1
+htpasswd -bc /etc/squid/passwd zhangsanfeng z1VrlD46V1Gj  >/dev/null 2>&1
 
 #增加自启动，运行
 systemctl enable squid
