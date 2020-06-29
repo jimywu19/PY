fb1fec4661ea5f4abbaa5ba9a2e19e812c0904fd
jimywu19 <fqy471@qq.com>
2020-06-30 00:04:05 +0800

    完善脚本，已经可以安装fastdfs

--- shell 脚本/install.sh
+++ shell 脚本/install.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
-# install fastdfs
 
 FASTDFS_BASE=/home/wb/fastdfs
 FASTDFS_TMP=/home/wb/tmp
 LOCAL_IP=$(ifconfig eth0 |grep -w inet|awk '{print $2}')
 HTTP_PORT=8888
 
+#颜色字体
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+#当前用户ROOT判断,非root打印提示信息并退出
+[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] This script must be run as root!" && exit 1
+
+yum install -y wget
+
+# install fastdfs
+
 [ -d $FASTDFS_BASE ] || mkdir -p $FASTDFS_BASE/{client,data,logs}
 [ -d $FASTDFS_TMP ] || mkdir -p $FASTDFS_TMP
 
+#下载安装文件源代码
 cd $FASTDFS_TMP
 wget http://183.131.202.100:8100/soft2/FastDFS/fastdfs-5.11.tar.gz
 wget http://183.131.202.100:8100/soft2/FastDFS/libfastcommon-1.0.39.tar.gz
@@ -27,6 +40,10 @@ cd libfastcommon-1.0.39
 cd ../fastdfs-5.11
 ./make.sh && ./make.sh install
 
+cp ./conf/* /etc/fdfs/
+cp $FASTDFS_TMP/fastdfs-nginx-module-1.20/src/mod_fastdfs.conf /etc/fdfs
+
+#安装nginx
 cd ../nginx-1.16.0
 sed -i "s#/usr/local/include#/usr/include/fastdfs /usr/include/fastcommon/#"  $FASTDFS_TMP/fastdfs-nginx-module-1.20/src/config
 ./configure \
@@ -50,6 +67,7 @@ sed -i "s#/usr/local/include#/usr/include/fastdfs /usr/include/fastcommon/#"  $F
 make && make install
 ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
 
+#创建更新nginx配置文件
 cat >/usr/local/nginx/conf/nginx.conf <<EOF
 worker_processes  1;
 events {
@@ -75,9 +93,26 @@ http {
 }
 EOF
 
+#创建systemctl启动服务文件
+cat >/usr/lib/systemd/system/nginx.service << EOF
+[Unit]
+Description=nginx service
+After=network.target 
+ 
+[Service] 
+Type=forking
+# 路径对应安装路径
+ExecStart=/usr/local/nginx/sbin/nginx
+ExecReload=/usr/local/nginx/sbin/nginx -s reload
+ExecStop=/usr/local/nginx/sbin/nginx -s quit
+PrivateTmp=true 
+ 
+[Install] 
+WantedBy=multi-user.target
+EOF
 
-cp ./conf/* /etc/fdfs/
 
+#修改配置文件
 sed -i "s#base_path=.*#base_path=$FASTDFS_BASE#" /etc/fdfs/tracker.conf
 sed -i "s#http.server_port=8080#http.server_port=$HTTP_PORT#" /etc/fdfs/tracker.conf
 
@@ -85,13 +120,23 @@ sed -i "s#base_path=.*#base_path=$FASTDFS_BASE#" /etc/fdfs/storage.conf
 sed -i "s#store_path0=.*#store_path0=$FASTDFS_BASE#" /etc/fdfs/storage.conf
 sed -i "s#tracker_server=.*#tracker_server=$LOCAL_IP:22122#" /etc/fdfs/storage.conf
 
-
 sed -i "s#base_path=.*#base_path=$FASTDFS_BASE/client#" /etc/fdfs/client.conf
 sed -i "s#tracker_server=.*#tracker_server=$LOCAL_IP:22122#" /etc/fdfs/client.conf
 sed -i "s#http\.tracker_server_port=.*#http\.tracker_server_port=$HTTP_PORT#" /etc/fdfs/client.conf
 
+sed -i "s#tracker_server=.*#tracker_server=$LOCAL_IP:22122#" /etc/fdfs/mod_fastdfs.conf
+sed -i "s#store_path0=.*#store_path0=$FASTDFS_BASE#" /etc/fdfs/mod_fastdfs.conf
 
+#启动服务
 fdfs_trackerd /etc/fdfs/tracker.conf start
 fdfs_storaged /etc/fdfs/storage.conf start
+systemctl enable nginx
+systemctl start nginx 
 
-
+#添加防火墙端口
+systemctl enable firewalld
+systemctl start firewalld
+firewall-cmd --add-port=${HTTP_PORT}/tcp --permanent
+firewall-cmd --add-port=22122/tcp --permanent
+firewall-cmd --add-port=23000/tcp --permanent
+firewall-cmd --reload
