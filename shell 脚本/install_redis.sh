067a21eb4249670447d11e855c0fc969648e205a
jimywu19 <fqy471@qq.com>
2020-07-02 17:39:10 +0800

    安装redis

---
+++ shell 脚本/install_redis.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+[ ! -x /usr/bin/wget ] && yum install -y wget
+[ ! -x /usr/bin/vim ] && yum install -y vim
+
+#install redis server
+REDIS_tmp="/tmp/redis"
+REDIS_PASSWD=""
+
+[ ! -d $REDIS_tmp ] && mkdir -p $REDIS_tmp
+cd $REDIS_tmp
+
+wget http://183.131.202.100:8100/soft2/redis-3.2.1.tar.gz
+tar xf redis-3.2.1.tar.gz
+cd redis-3.2.1
+make && make install
+cp redis.conf /etc/redis.conf
+
+sed -i "s/daemonize no/daemonize yes/" /etc/redis.conf 
+sed -i "s/# requirepass .*/requirepass $REDIS_PASSWD/" /etc/redis.conf
+
+
+sed -i "s#^net.core.somaxconn.*#net.core.somaxconn=1024#" /etc/sysctl.conf
+sed -d "s#^vm.overcommit_memory.*#vm.overcommit_memory=1#" /etc/sysctl.conf
+
+echo "net.core.somaxconn=1024" >> /etc/sysctl.conf
+echo "vm.overcommit_memory=1" >> /etc/sysctl.conf
+sysctl -p
+
+echo "never > /sys/kernel/mm/transparent_hugepage/enabled" >>/etc/rc.local
+
+redis-server /etc/redis.conf
