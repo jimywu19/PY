bcc8d3079915c6b5157c89bb857e6b2d3236f3d1
jimywu19 <fqy471@qq.com>
2020-08-14 10:53:50 +0800

    final

--- text.py
+++ text.py
@@ -1,37 +1,29 @@
 from os.path import abspath
-import sys
-import time
 import re
 import os
 
-
-time1 = time.time()
-# print("初始时间为：",time1)
-
-import os.path
 from pdfminer.pdfparser import  PDFParser, PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter, PDFTextExtractionNotAllowed
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LTTextBoxHorizontal,LAParams
-from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+import camelot
 
+##  Constants
 ROOT_DIR='D:\\Lenovo\\Documents\\py'
-EIP = list()
-
-
-# text_path = r'words-words.pdf'
-#text_path = r'IP置换交付单-罗滨20200103E1.pdf'
+#text_path = r'words-words.pdf'
+text_path = r'20义数云平台资源交付单--何旭1008_427309286238969.pdf'
+outfile = '2.txt'
 
 def getIp(text):
     '''解析字段中的IP地址'''
-    text
-    result = re.findall(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", text)
-    if result: 
-        return result
+    
+    ipaddress = re.findall(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", text)
+    if ipaddress: 
+        return ipaddress
 
-
-def parse(file):
+def parse(file,savefile):
     '''解析PDF文本，并保存到TXT文件中'''
+    EIP = []
     fp = open(file,'rb')
     #用文件对象创建一个PDF文档分析器
     parser = PDFParser(fp)
@@ -57,8 +49,7 @@ def parse(file):
         # doc.get_pages() 获取page列表
         for page in doc.get_pages():
             try :
-                interpreter.process_page(page)
-            
+                interpreter.process_page(page)            
                 #接受该页面的LTPage对象
                 layout = device.get_result()
                 # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象
@@ -72,18 +63,18 @@ def parse(file):
                             EIP.extend(ipAddress)
             except:
                 continue
+    fp.close
 
-        with open('2.txt','a') as f:
-            for line in EIP:
-                f.write(line+"\n")
-
+    with open(savefile,'a') as f:
+        for line in EIP:
+            f.write(line+"\n")
 
 if __name__ == '__main__':
-    parse("20义数云平台资源交付单--何旭1008_427309286238969.pdf")
+
+    parse(text_path,outfile)
     # for root,dirs,files in os.walk(ROOT_DIR):
     #     for filename in files:
     #         if filename.endswith('.pdf'):
     #             absname = os.path.join(root,filename)
-    #             parse(absname)
-    time2 = time.time()
-    print("总共消耗时间为:",time2-time1)
+    #             parse(absname,outfile)
+
